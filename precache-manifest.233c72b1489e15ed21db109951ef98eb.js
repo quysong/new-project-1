f933c2051db19cb2f9b28f79de88306d12ee4fb0
anh.ly <anh.ly@vitopmedia.co>
2020-01-31 13:35:17 +0700

    Updates

---
+++ precache-manifest.233c72b1489e15ed21db109951ef98eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53085d89edb51b0f8b3975138b9b6b9",
+    "url": "/new-project-1/index.html"
+  },
+  {
+    "revision": "8dec5b5c0e36575ae202",
+    "url": "/new-project-1/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6d0c921777cd39a6d34",
+    "url": "/new-project-1/static/js/2.cc377f63.chunk.js"
+  },
+  {
+    "revision": "8dec5b5c0e36575ae202",
+    "url": "/new-project-1/static/js/main.edc087ea.chunk.js"
+  },
+  {
+    "revision": "3e2fba56505c69279c93",
+    "url": "/new-project-1/static/js/runtime-main.2fe42afc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/new-project-1/static/media/logo.25bf045c.svg"
+  }
+]);
